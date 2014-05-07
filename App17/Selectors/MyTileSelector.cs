ee0aaaf38955d7268061c44fe4abac26ebb3399c
Aleksandr Biryukov <aleksbiryukov@gmail.com>
2014-05-08 04:04:53 +0700

    MyTileSelector works

--- App17/Selectors/MyTileSelector.cs
+++ App17/Selectors/MyTileSelector.cs
@@ -18,21 +18,12 @@ namespace App17.Selectors
             SampleDataItem selectedItem = item as SampleDataItem;
             String template;
             
-            if (true)
+            if (selectedItem.Icon == "")
             {
                 template = @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
                 <Grid HorizontalAlignment=""Left"" Width=""250"" Height=""250"">
                     <Border Background=""{StaticResource ListViewItemPlaceholderBackgroundThemeBrush}"">
-                        <Grid>
-                            <Image Source=""{Binding ImagePath}"" Stretch=""UniformToFill"" AutomationProperties.Name=""{Binding Title}""/>
-                            <Viewbox Margin=""10"" HorizontalAlignment=""Center"" VerticalAlignment=""Center"">
-                                <Path Fill=""{StaticResource MyBlueGrafient}"" Data=""{Binding Icon}"" 
-                                                            Stretch=""Uniform""  
-                                                            Width=""26"" Height=""26"" 
-                                                            Margin=""0"" RenderTransformOrigin=""0.5,0.5"">
-                                </Path>
-                            </Viewbox>
-                        </Grid>
+                        <Image Source=""{Binding ImagePath}"" Stretch=""UniformToFill"" AutomationProperties.Name=""{Binding Title}""/>
                     </Border>
                     <StackPanel VerticalAlignment=""Bottom"" Background=""{StaticResource ListViewItemOverlayBackgroundThemeBrush}"">
                         <TextBlock Text=""{Binding Title}"" Foreground=""{StaticResource ListViewItemOverlayForegroundThemeBrush}"" Style=""{StaticResource TitleTextStyle}"" Height=""60"" Margin=""15,0,15,0""/>
@@ -44,8 +35,22 @@ namespace App17.Selectors
             else
             {
                 template = @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
-                    <TextBox Text=""Элемент отображается неправильно"" FontSize=""50"" />
-                </DataTemplate>";
+                <Grid HorizontalAlignment=""Left"" Width=""250"" Height=""250"">
+                    <Border Background=""{StaticResource ListViewItemPlaceholderBackgroundThemeBrush}"">
+                        <Viewbox Margin=""10"" HorizontalAlignment=""Center"" VerticalAlignment=""Center"">
+                            <Path Fill=""{StaticResource MyBlueGrafient}"" Data=""{Binding Icon}"" 
+                                                        Stretch=""Uniform""  
+                                                        Width=""26"" Height=""26"" 
+                                                        Margin=""0"" RenderTransformOrigin=""0.5,0.5"">
+                            </Path>
+                        </Viewbox>
+                    </Border>
+                    <StackPanel VerticalAlignment=""Bottom"" Background=""{StaticResource ListViewItemOverlayBackgroundThemeBrush}"">
+                        <TextBlock Text=""{Binding Title}"" Foreground=""{StaticResource ListViewItemOverlayForegroundThemeBrush}"" Style=""{StaticResource TitleTextStyle}"" Height=""60"" Margin=""15,0,15,0""/>
+                        <TextBlock Text=""{Binding Subtitle}"" Foreground=""{StaticResource ListViewItemOverlaySecondaryForegroundThemeBrush}"" Style=""{StaticResource CaptionTextStyle}"" TextWrapping=""NoWrap"" Margin=""15,0,15,10""/>
+                    </StackPanel>
+                </Grid>
+            </DataTemplate>";
             }
 
             return XamlReader.Load(template) as DataTemplate;
