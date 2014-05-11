b2a9315bd11f531bfcb93b1d1156e09c6d93243e
Aleksandr Biryukov <aleksbiryukov@gmail.com>
2014-05-11 20:19:30 +0700

    add logo brushes

--- App17/Selectors/MyTheme.cs
+++ App17/Selectors/MyTheme.cs
@@ -18,11 +18,22 @@ namespace App17.Selectors
         public Brush TileFrontBrush { get; set; }
         public Brush PopupBrush { get; set; }
 
+
         private static List<MyTheme> myThemeStaticList = new List<MyTheme>() 
                 {
                     new MyTheme()
                     {
-                        LogoBrush = new SolidColorBrush(Colors.Pink),
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF081E4D"" Offset=""0""/>
+                                <GradientStop Color=""#FF1747B0"" Offset=""0.23""/>
+                                <GradientStop Color=""#FF2161EC"" Offset=""0.433""/>
+                                <GradientStop Color=""#FF5A8AF1"" Offset=""0.77""/>
+                                <GradientStop Color=""#FFC0CFF0"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
                         BackBrush = new SolidColorBrush(Colors.Blue),
                         TileBackBrush = new SolidColorBrush(Colors.Green),
                         TileFrontBrush = new SolidColorBrush(Colors.Red),
@@ -30,7 +41,17 @@ namespace App17.Selectors
                     },
                     new MyTheme()
                     {
-                        LogoBrush = new SolidColorBrush(Colors.Brown),
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF360500"" Offset=""0""/>
+                                <GradientStop Color=""#FF680A00"" Offset=""0.23""/>
+                                <GradientStop Color=""#FFA41000"" Offset=""0.433""/>
+                                <GradientStop Color=""#FFFB1900"" Offset=""0.77""/>
+                                <GradientStop Color=""#FFFFCFC0"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
                         BackBrush = new SolidColorBrush(Colors.Red),
                         TileBackBrush = new SolidColorBrush(Colors.YellowGreen),
                         TileFrontBrush = new SolidColorBrush(Colors.WhiteSmoke),
@@ -38,7 +59,17 @@ namespace App17.Selectors
                     },
                     new MyTheme()
                     {
-                        LogoBrush = new SolidColorBrush(Colors.Green),
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF013403"" Offset=""0""/>
+                                <GradientStop Color=""#FF005503"" Offset=""0.23""/>
+                                <GradientStop Color=""#FF007C04"" Offset=""0.433""/>
+                                <GradientStop Color=""#FF00A805"" Offset=""0.77""/>
+                                <GradientStop Color=""#FFC7F0C0"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
                         BackBrush = new SolidColorBrush(Colors.Orange),
                         TileBackBrush = new SolidColorBrush(Colors.White),
                         TileFrontBrush = new SolidColorBrush(Colors.Olive),
@@ -46,7 +77,17 @@ namespace App17.Selectors
                     },
                     new MyTheme()
                     {
-                        LogoBrush = new SolidColorBrush(Colors.Pink),
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF4D2408"" Offset=""0""/>
+                                <GradientStop Color=""#FFB07F17"" Offset=""0.23""/>
+                                <GradientStop Color=""#FFEC9921"" Offset=""0.433""/>
+                                <GradientStop Color=""#FFF1C85A"" Offset=""0.77""/>
+                                <GradientStop Color=""#FFF0E9C0"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
                         BackBrush = new SolidColorBrush(Colors.Blue),
                         TileBackBrush = new SolidColorBrush(Colors.Green),
                         TileFrontBrush = new SolidColorBrush(Colors.Red),
@@ -54,7 +95,17 @@ namespace App17.Selectors
                     },
                     new MyTheme()
                     {
-                        LogoBrush = new SolidColorBrush(Colors.Pink),
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF084A4D"" Offset=""0""/>
+                                <GradientStop Color=""#FF17A9B0"" Offset=""0.23""/>
+                                <GradientStop Color=""#FF21ECE3"" Offset=""0.433""/>
+                                <GradientStop Color=""#FF5AEAF1"" Offset=""0.77""/>
+                                <GradientStop Color=""#FFC0EEF0"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
                         BackBrush = new SolidColorBrush(Colors.Blue),
                         TileBackBrush = new SolidColorBrush(Colors.Green),
                         TileFrontBrush = new SolidColorBrush(Colors.Red),
@@ -62,7 +113,17 @@ namespace App17.Selectors
                     },
                     new MyTheme()
                     {
-                        LogoBrush = new SolidColorBrush(Colors.Pink),
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF40084D"" Offset=""0""/>
+                                <GradientStop Color=""#FFA217B0"" Offset=""0.23""/>
+                                <GradientStop Color=""#FFBE21EC"" Offset=""0.433""/>
+                                <GradientStop Color=""#FFF15AE3"" Offset=""0.77""/>
+                                <GradientStop Color=""#FFF0C0EC"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
                         BackBrush = new SolidColorBrush(Colors.Blue),
                         TileBackBrush = new SolidColorBrush(Colors.Green),
                         TileFrontBrush = new SolidColorBrush(Colors.Red),
